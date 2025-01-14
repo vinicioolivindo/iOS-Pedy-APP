5942d85d76183d1fe2e612a9c402dade13bc8cbd
Thalia Santos <thaliasantosfd@gmail.com>
2025-01-14 15:08:54 -0300

    primeira tela concluida

---
+++ iOS-Pedy-APP/View/RegisterView/Componets RegisterView/CustomPickerView.swift
@@ -0,0 +1,18 @@
+//
+//  CustomPickerView.swift
+//  iOS-Pedy-APP
+//
+//  Created by iredefbmac_23 on 14/01/25.
+//
+
+import SwiftUI
+
+struct CustomPickerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomPickerView()
+}
