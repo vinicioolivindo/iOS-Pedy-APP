0e273470b3a84aa02321f4642a90a4dc3c024385
Thalia Santos <thaliasantosfd@gmail.com>
2025-01-17 18:57:06 -0300

    component bar add

--- iOS-Pedy-APP/View/GeneralComponents/NavigationBarView.swift
+++ iOS-Pedy-APP/View/GeneralComponents/NavigationBarView.swift
@@ -1,18 +1,18 @@
 //
-//  StartView.swift
+//  NavigationBarView.swift
 //  iOS-Pedy-APP
 //
-//  Created by iredefbmac_23 on 16/01/25.
+//  Created by iredefbmac_23 on 17/01/25.
 //
 
 import SwiftUI
 
-struct StartView: View {
+struct NavigationBarView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    StartView()
+    NavigationBarView()
 }
